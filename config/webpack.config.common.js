feafb20970388738bd64bc4c88c278f4dbb4ab7c
Gleb Chekushin <mail@glebchek.com>
2022-05-08 17:40:56 +0500

    Init toolchain

---
+++ config/webpack.config.common.js
@@ -0,0 +1,52 @@
+// Plugins
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+// Local
+const paths = require('./paths');
+
+module.exports = {
+  entry: paths.appIndex,
+  output: {
+    path: paths.appDist,
+    filename: '[name].[contenthash:8].js',
+    publicPath: '/',
+  },
+  target: 'web',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Homepage',
+      template: paths.appTemplateHtml,
+    }),
+    new ESLintPlugin({
+      context: paths.appSrc,
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)s$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+        },
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[name][ext]',
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]',
+        },
+      },
+    ],
+  },
+};
