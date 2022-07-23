ebaf4271b5fbde8f07fae2bd1d69550d44de83c9
Gleb Chekushin <mail@glebchek.com>
2022-07-23 08:43:59 +0500

    Add with-sidebar component

---
+++ src/components/with-sidebar.ts
@@ -0,0 +1,27 @@
+import { LitElement, html, css } from 'lit';
+
+export class MyElement extends LitElement {
+  static styles = css`
+    :host {
+      display: flex;
+      flex-wrap: wrap;
+      gap: var(--s1);
+    }
+
+    ::slotted([slot='sidebar']) {
+      flex-grow: 1;
+    }
+
+    ::slotted([slot='content']) {
+      flex-basis: 0;
+      flex-grow: 999;
+      min-inline-size: 50%;
+    }
+  `;
+
+  render() {
+    return html`<slot name="sidebar"></slot><slot name="content"></slot>`;
+  }
+}
+
+customElements.define('with-sidebar', MyElement);
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { LitElement, html } from 'lit';
+import './components/with-sidebar';
 
 class MyElement extends LitElement {
   render() {
-    return html`<h1>Hello World!</h1>`;
+    return html`
+      <with-sidebar>
+        <header slot="sidebar">sidebar</header>
+        <main slot="content">contetnt</main>
+      </with-sidebar>
+    `;
   }
 }
 
