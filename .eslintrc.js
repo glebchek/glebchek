b26270656cbe67e9ac4af7e5b8e776d2bd3a611d
Gleb Chekushin <mail@glebchek.com>
2023-01-30 06:20:42 +0500

    Add eslint svelte configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,7 @@ const baseConfigList = [
   '@spotify/eslint-config-base',
 ];
 
-const jsConfigList = [
-  ...baseConfigList,
-  'prettier',
-];
+const jsConfigList = [...baseConfigList, 'prettier'];
 const tsConfigList = [
   ...baseConfigList,
   '@spotify/eslint-config-typescript',
@@ -33,6 +30,18 @@ module.exports = {
         sourceType: 'module',
       },
       settings: tsSettings,
+    },
+    {
+      files: ['*.svelte'],
+      plugins: ['svelte3'],
+      processor: 'svelte3/svelte3',
+      extends: tsConfigList,
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: 'tsconfig.json',
+        sourceType: 'module',
+      },
+      settings: { ...tsSettings, 'svelte3/typescript': true },
     },
   ],
 };
