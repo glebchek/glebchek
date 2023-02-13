e5719ef086b3e92615f61209fe59ebcb7de345ff
Gleb Chekushin <mail@glebchek.com>
2023-02-14 02:04:39 +0500

    Add Stack component

---
+++ src/components/common/Stack.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let stackSpace: string;
+</script>
+
+<div class="stack" style:--stack-space={stackSpace}><slot /></div>
+
+<style>
+  .stack {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+  }
+
+  .stack > ::global(*) {
+    margin-block: 0;
+  }
+
+  .stack > :global(* + *) {
+    margin-block-start: var(--stack-space);
+  }
+</style>
