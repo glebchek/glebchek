675ba06173173585b930c563f0fe80454a5b83fa
Gleb Chekushin <mail@glebchek.com>
2023-11-06 12:13:57 +0500

    Add Button

---
+++ src/components/common/Button.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export const title = 'Press Me';
+</script>
+
+<button>{title}</button>
+
+<style>
+  button {
+    background-color: transparent;
+    border: 0.11rem solid var(--color-2);
+    box-shadow: var(--shadow-elevation-medium);
+
+    margin: var(--space-3xs-2xs);
+    padding: var(--space-3xs-2xs);
+  }
+
+  button:active {
+    box-shadow: var(--shadow-elevation-low);
+  }
+</style>
