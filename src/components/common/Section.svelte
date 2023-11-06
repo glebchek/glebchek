e3c3a0991aa638f9b7b979970b810909d7d3f905
Gleb Chekushin <mail@glebchek.com>
2023-11-06 18:42:22 +0500

    Add Section component

---
+++ src/components/common/Section.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let title: string;
+</script>
+
+<section>
+  <h2>{title}</h2>
+  <div><slot /></div>
+</section>
+
+<style>
+  h2 {
+    font-weight: normal;
+  }
+</style>
